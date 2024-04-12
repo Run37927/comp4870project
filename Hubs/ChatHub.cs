f12bfe213278a5f7826687917baff39e817951fe
Hairun Huang <hairunhuang@gmail.com>
2024-04-12 11:58:18 -0700

    fix warning

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,3 +1,6 @@
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor.
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed.
 using System;
 using System.Collections.Generic;
 using System.Linq;
